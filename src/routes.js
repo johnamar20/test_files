eb5487acde6bbf25e3566cb9e758647df9978653
johnamar20 <9245758569a>
2021-02-05 03:04:38 -0800

    Add All Files

---
+++ src/routes.js
@@ -0,0 +1,49 @@
+/*!
+
+=========================================================
+* Light Bootstrap Dashboard React - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import Dashboard from "views/Dashboard.js";
+import UserProfile from "views/UserProfile.js";
+import Events from "views/Events.js";
+
+const dashboardRoutes = [
+  
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "nc-icon nc-chart-pie-35",
+    component: Dashboard,
+    layout: "/admin",
+  },
+  
+  {
+    path: "/Events",
+    name: "EVENTS",
+    icon: "nc-icon nc-notes",
+    component: Events,
+    layout: "/admin",
+  },
+  {
+    path: "/user",
+    name: "USERS",
+    icon: "nc-icon nc-circle-09",
+    component: UserProfile,
+    layout: "/admin",
+  },
+  
+];
+
+export default dashboardRoutes;
---
+++ src/views/Eventreg.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Eventreg() {
+    return (
+        <div>
+            <h1>EVENTS</h1>
+        </div>
+    )
+}
+
+export default Eventreg
+
